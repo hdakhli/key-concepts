8bb50ea298cff2b0429e8cf88e2eff2a5b17cf46
Hicham DAKHLI <hicham.dakhli@axa.fr>
2022-11-07 19:06:03 +0100

    Update api and concurrency

--- 4_architecture/api/quiz_controller.py
+++ 4_architecture/api/quiz_controller.py
@@ -5,23 +5,24 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+@app.get("/say-hello")
 def say_hello(user_name: str) -> str:
     print(f"{user_name} connecté!")
     return f"Hello {user_name}"
 
 
-# class Quiz(BaseModel):
-#     user_name: str
-#     question_id: int
-#     answer_id: int
-#
-#
-# @app.post("/send-answer")
-# def answer(quiz: Quiz) -> bool:
-#     print(
-#         f"{quiz.user_name} a envoyé la réponse: {quiz.answer_id} "
-#         f"pour la question: {quiz.question_id}")
-#     return True
+class Quiz(BaseModel):
+    user_name: str
+    question_id: int
+    answer_id: int
+
+
+@app.post("/send-answer")
+def answer(quiz: Quiz) -> bool:
+    print(
+        f"{quiz.user_name} a envoyé la réponse: {quiz.answer_id} "
+        f"pour la question: {quiz.question_id}")
+    return True
 
 
 if __name__ == "__main__":
