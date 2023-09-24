c2d619b8b287dd7e92409fdfaa2a5103e9fdab39
Hicham DAKHLI <hicham.dakhli@axa.fr>
2023-09-24 09:21:39 +0200

    Remove architecture 4

--- 6_architecture/api/quiz_controller.py
+++ 6_architecture/api/quiz_controller.py
@@ -1,24 +1,20 @@
-import asyncio
-import time
-
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-import requests
 
 app = FastAPI()
-
-
-@app.get("/say-hello")
-def say_hello(user_name: str) -> str:
-    print(f"{user_name} connecté!")
-    return f"Hello {user_name}"
 
 
 class Quiz(BaseModel):
     user_name: str
     question_id: int
     answer_id: int
+
+
+@app.get("/say-hello")
+def say_hello(user_name: str) -> str:
+    print(f"{user_name} connecté!")
+    return f"Hello {user_name}"
 
 
 @app.post("/send-answer")
@@ -29,35 +25,5 @@ def answer(quiz: Quiz) -> bool:
     return True
 
 
-@app.get("/get-a-joke")
-def get_a_joke(user_name: str) -> list:
-    start_time = time.time()
-    dog_response = call_api('https://dog.ceo/api/breeds/image/random')
-    user_response = call_api('https://randomuser.me/api/')
-    joke_response = call_api(
-        'https://official-joke-api.appspot.com/random_joke')
-
-    print(f"{user_name} connecté!")
-
-    responses = [dog_response, joke_response, user_response]
-
-    end_time = time.time()
-    duration = end_time - start_time
-    print(f"Duration: {duration}")
-
-    return responses
-
-
-def call_api(url):
-    response = requests.get(url)
-    return response.json()
-
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
-
-
-
-
-
-# responses = await asyncio.gather(dog_response, joke_response, user_response)
