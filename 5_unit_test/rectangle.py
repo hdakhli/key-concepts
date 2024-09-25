e395ddc9cf8505f56ebe495ca861d605727e84d5
Hicham DAKHLI <hichamdakhli@gmail.com>
2024-09-25 14:24:07 +0100

    Implemebter rectangle

---
+++ 5_unit_test/rectangle.py
@@ -0,0 +1,10 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
